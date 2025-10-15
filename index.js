5d9912dc420a70a220151e51b6e81b7bab073742
ego5g <sat.open.world@gmail.com>
2025-10-15 13:07:57 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = process.env.8259422856:AAFfqhBOK_h9GGGtk5LQZBR2lpJE1LpJubI;
+const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  bot.sendMessage(chatId, `Привет Егорик! Ты написал: ${text}`);
+  bot.sendMessage(chatId, `Привет! Ты написал: ${text}`);
 });
