599d62fcc4d375ebc6386afd831492488028c72b
ego5g <sat.open.world@gmail.com>
2026-02-04 13:10:33 +0300

    Add healthcheck server to index.js

--- index.js
+++ index.js
@@ -472,3 +472,14 @@ process.on('SIGINT', () => {
   bot.stopPolling();
   process.exit(0);
 });
+
+import http from "http";
+
+const PORT = process.env.PORT || 8080;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("OK");
+}).listen(PORT, () => {
+  console.log(`🌐 Healthcheck server running on port ${PORT}`);
+});
